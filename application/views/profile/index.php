17dce38322f4862e1ccc522fa1ab2214ec54c4df
muhadimulyana <muhammadadimulyana30@gmail.com>
2020-10-28 17:09:01 +0700

    add controller profile and view

---
+++ application/views/profile/index.php
@@ -0,0 +1,32 @@
+<!-- Hero Start -->
+<section class="bg-half-170 d-table w-100" style="background: url('images/travel/bg.jpg') center center;">
+	<div class="bg-overlay"></div>
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-lg-7 col-md-6">
+				<div class="title-heading mt-4">
+					<h1 class="display-4 font-weight-bold text-white title-dark mb-3">Booking tour <br> made easy</h1>
+					<p class="para-desc text-white-50">Launch your campaign and benefit from our expertise on designing
+						and managing conversion centered bootstrap4 html page.</p>
+					<div class="mt-4">
+						<a href="javascript:void(0)" class="btn btn-icon btn-pills btn-lg btn-light"><i
+								data-feather="arrow-down" class="icons"></i></a>
+					</div>
+				</div>
+			</div>
+			<!--end col-->
+
+			<div class="col-lg-5 col-md-6 mt-4 pt-2 mt-sm-0 pt-sm-0">
+				<div class="card login_page shadow rounded border-0 ml-lg-5">
+					<div class="card-body">
+						<h5 class="card-title">You can start search here</h5>
+					</div>
+				</div>
+			</div>
+			<!--end col-->
+		</div>
+		<!--end row-->
+	</div>
+	<!--end container-->
+</section>
+<!--end section-->
