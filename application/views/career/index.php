a33e8e874db4bf614bda1fd988a51d8788615490
muhadimulyana <muhammadadimulyana30@gmail.com>
2020-11-02 14:40:54 +0700

    modify index career page

--- application/views/career/index.php
+++ application/views/career/index.php
@@ -1,8 +1,65 @@
+<style>
+	#topnav {
+		background-color: rgb(104, 110, 154, 0.65);
+	}
+
+    .text-xl-career {
+        font-size: 100px;
+    }
+
+    .bg-overlay-navy-300 {
+			background-color: rgba(15, 15, 140, 0.45);
+			/* background-color: rgba(60, 72, 88, 0.7); */
+			position: absolute;
+			top: 0;
+			right: 0;
+			left: 0;
+			bottom: 0;
+			width: 100%;
+			height: 100%;
+		}
+
+</style>
+
+
 <!-- Product Start -->
 <section class="section"
-	style="background-image:url('<?= base_url(); ?>assets/images/product/background 2.png'); background-size: cover; min-height: 500px; background-position: center;">
+	style="background-image:url('<?= base_url(); ?>assets/images/career/all.jpeg'); background-size: cover; min-height: 500px; background-position: center;">
 	<div class="bg-overlay-navy"></div>
-	<div class="container pb-lg-4 mb-md-5">
+	<div class="container pb-lg-4 mb-md-5 mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-12 text-center">
+                <h1 class="heading text-white m-0">We Are <span class="text-xl-career text-white">Hiring</span></h1>
+                
+            </div>
+        </div>
+    </div>
+</section>
 
+<section class="section bg-white" >
+    <div class="container p-lg-4">
+        <div class="row">
+            <div class="col-md-6 order-2 order-md-1">
+                <h5 class="text-navy">Gabung Bersama</h5>
+                <h1 class="text-navy">Pan Era Group</h5>
+                <p class="text-navy">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            </div>
+            <div class="col-md-6 order-1 order-md-2 mb-2 text-center">
+                <img src="<?= base_url(); ?>assets/images/career/vector.png" class="img-fluid" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="section" style="background-image:url('<?= base_url(); ?>assets/images/career/work.jpeg'); background-size: cover; min-height: 500px; background-position: center;">
+    <div class="bg-overlay-navy-300"></div>
+    <div class="container justify-content-center p-5">
+        <div class="row text-center">
+            <div class="col-md-12">
+                <h5 class="text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur <br><br> Lectus sit amet est placerat in:</h5>
+                <a href="javascript:void(0)" class="btn btn-light btn-pills btn-xl">hrd@paneragroup.com</a><br>
+                <small class="text-white">Ut tellus elementum sagittis vitae</small>
+                <h5 class="text-white mt-3">Amet tellus cras adipiscing enim eu turpis egestas. Sed velit dignissim sodales ut eu sem integer vitae. Turpis nunc eget lorem dolor sed viverra ipsum nunc aliquet.</h5>
+        </div>
     </div>
 </section>
