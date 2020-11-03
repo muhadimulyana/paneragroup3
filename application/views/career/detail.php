60a3321a7a022a6996ac9afaa28eed2c83887033
muhadimulyana <muhammadadimulyana30@gmail.com>
2020-11-03 11:09:08 +0700

    add detail job page

---
+++ application/views/career/detail.php
@@ -0,0 +1,185 @@
+<style>
+	#topnav {
+		background-color: rgb(104, 110, 154, 0.4);
+	}
+
+	.bg-transparent {
+		background-color: rgb(255, 255, 255, 0) !important;
+	}
+
+</style>
+<!-- Product Start -->
+<section class="section"
+	style="background-image:url('<?= base_url(); ?>assets/images/career/bg.png'); background-size: cover; min-height: 500px; background-position: center;">
+	<div class="container pb-lg-4 mb-md-5 mt-5">
+		<div class="row">
+			<div class="col-md-6 order-2 order-md-1">
+				<h1>PAN ERA GROUP</h1>
+				<h4>ORIENTASI KAMI ADALAH KEGIATAN PENDAURAN ULANG PLASTIK</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+					aliquip ex ea commodo consequat.</p>
+				<div class="text-center">
+					<a href="javascrip:void" class="btn btn-navy btn-pills text-center"
+						style="font-size: 25px;">www.paneragroup.com</a>
+				</div>
+			</div>
+			<div class="col-md-6 order-1 order-md-2 text-center">
+				<img src="<?= base_url(); ?>assets/images/career/vector-01.svg" class="img-fluid" alt="">
+			</div>
+		</div>
+	</div>
+	<div class="col-12 p-0">
+		<h5 class="text-center p-2 text-white mb-0 text-capitalize" style="background-color:rgb(7, 2, 76, 0.8); ">
+			Excepteur sint
+			occaecat cupidatat non proident sunt in culpa qui officia <br> deserunt mollit anim id est laborum.</h5>
+	</div>
+	<div class="container mt-5">
+		<div class="row border border-success p-3">
+			<div class="col-lg-4 col-md-5 col-12">
+				<div class="card sidebar rounded bg-transparent border-0">
+					<div class="card-body widget border-bottom">
+						<h5 class="mb-0">Job Information</h5>
+					</div>
+
+					<div class="card-body">
+						<div class="media widget align-items-center">
+							<i data-feather="user-check" class="fea icon-ex-md mr-3"></i>
+							<div class="media-body">
+								<h4 class="widget-title mb-0">Employee Type:</h4>
+								<p class="text-primary mb-0">Full Time</p>
+							</div>
+						</div>
+
+						<div class="media widget align-items-center mt-3">
+							<i data-feather="map-pin" class="fea icon-ex-md mr-3"></i>
+							<div class="media-body">
+								<h4 class="widget-title mb-0">Location:</h4>
+								<p class="text-primary mb-0">Cikarang, Jawa Barat, Indonesia</p>
+							</div>
+						</div>
+
+						<div class="media widget align-items-center mt-3">
+							<i data-feather="monitor" class="fea icon-ex-md mr-3"></i>
+							<div class="media-body">
+								<h4 class="widget-title mb-0">Job Type:</h4>
+								<p class="text-primary mb-0">Back-end Developer</p>
+							</div>
+						</div>
+
+						<div class="media widget align-items-center mt-3">
+							<i data-feather="briefcase" class="fea icon-ex-md mr-3"></i>
+							<div class="media-body">
+								<h4 class="widget-title mb-0">Experience:</h4>
+								<p class="text-primary mb-0">+2 Year</p>
+							</div>
+						</div>
+
+						<div class="media widget align-items-center mt-3">
+							<i data-feather="book" class="fea icon-ex-md mr-3"></i>
+							<div class="media-body">
+								<h4 class="widget-title mb-0">Qualifications:</h4>
+								<p class="text-primary mb-0">Bachelor Degree</p>
+							</div>
+						</div>
+
+						<div class="media widget align-items-center mt-3">
+							<i data-feather="dollar-sign" class="fea icon-ex-md mr-3"></i>
+							<div class="media-body">
+								<h4 class="widget-title mb-0">Salary:</h4>
+								<p class="text-primary mb-0">-</p>
+							</div>
+						</div>
+
+						<div class="media widget align-items-center mt-3">
+							<i data-feather="clock" class="fea icon-ex-md mr-3"></i>
+							<div class="media-body">
+								<h4 class="widget-title mb-0">Date posted:</h4>
+								<p class="text-primary mb-0 mb-0">5th Oct, 2020</p>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<!--end col-->
+
+			<div class="col-lg-8 col-md-7 col-12 mt-4 mt-sm-0 pt-2 pt-sm-0">
+				<div class="ml-lg-4">
+					<h5>Job Description: </h5>
+					<p>One disadvantage of Lorum Ipsum is that in Latin certain letters appear more
+						frequently than others - which creates a distinct visual impression. Moreover, in Latin only
+						words at the beginning of sentences are capitalized.</p>
+					<p>This means that Lorem Ipsum cannot accurately represent, for example, German,
+						in which all nouns are capitalized. Thus, Lorem Ipsum has only limited suitability as a visual
+						filler for German texts. If the fill text is intended to illustrate the characteristics of
+						different typefaces.</p>
+					<p>It sometimes makes sense to select texts containing the various letters and
+						symbols specific to the output language.</p>
+
+					<h5 class="mt-4">Responsibilities and Duties: </h5>
+					<p>It sometimes makes sense to select texts containing the various letters and
+						symbols specific to the output language.</p>
+					<ul class="list-unstyled">
+						<li><i data-feather="check-square" class="fea icon-sm mr-2"></i>Participate in
+							requirements analysis</li>
+						<li><i data-feather="check-square" class="fea icon-sm mr-2"></i>Write clean,
+							scalable code using C# and .NET
+							frameworks</li>
+						<li><i data-feather="check-square" class="fea icon-sm mr-2"></i>Test and deploy
+							applications and systems</li>
+						<li><i data-feather="check-square" class="fea icon-sm mr-2"></i>Revise, update,
+							refactor and debug code</li>
+						<li><i data-feather="check-square" class="fea icon-sm mr-2"></i>Improve existing
+							software</li>
+						<li><i data-feather="check-square" class="fea icon-sm mr-2"></i>Develop
+							documentation throughout the software
+							development life cycle (SDLC)</li>
+						<li><i data-feather="check-square" class="fea icon-sm mr-2"></i>Serve as an expert
+							on applications and provide
+							technical support</li>
+					</ul>
+
+					<h5 class="mt-4">Required Experience, Skills and Qualifications: </h5>
+					<p>It sometimes makes sense to select texts containing the various letters and
+						symbols specific to the output language.</p>
+					<ul class="list-unstyled">
+						<li><i data-feather="check-square" class="fea icon-sm mr-2"></i>Proven experience
+							as
+							a .NET Developer or
+							Application Developer</li>
+						<li><i data-feather="check-square" class="fea icon-sm mr-2"></i>good understanding
+							of SQL and Relational
+							Databases, specifically Microsoft SQL Server.</li>
+						<li><i data-feather="check-square" class="fea icon-sm mr-2"></i>Experience
+							designing, developing and creating
+							RESTful web services and APIs</li>
+						<li><i data-feather="check-square" class="fea icon-sm mr-2"></i>Basic know how of
+							Agile process and practices
+						</li>
+						<li><i data-feather="check-square" class="fea icon-sm mr-2"></i>Good understanding
+							of object-oriented
+							programming.</li>
+						<li><i data-feather="check-square" class="fea icon-sm mr-2"></i>Good understanding
+							of concurrent programming.
+						</li>
+						<li><i data-feather="check-square" class="fea icon-sm mr-2"></i>Sound knowledge of
+							application architecture
+							and design.</li>
+						<li><i data-feather="check-square" class="fea icon-sm mr-2"></i>Excellent problem
+							solving and analytical
+							skills</li>
+					</ul>
+
+					<div class="mt-4">
+						<a href="javascript:void(0)" class="btn text-white btn-pills"
+							style="background-color: #282c73 !important;">hrd.era@paneragroup.com</a>
+					</div>
+				</div>
+			</div>
+			<!--end col-->
+		</div>
+		<!--end row-->
+	</div>
+	<!--end container-->
+</section>
