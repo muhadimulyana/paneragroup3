a104d8d74f2121f92c3b3b741439434c4a2373e4
muhadimulyana <muhammadadimulyana30@gmail.com>
2020-11-04 16:27:23 +0700

    modified profile page

--- application/views/profile/index.php
+++ application/views/profile/index.php
@@ -1,20 +1,24 @@
 <style>
 	#topnav {
-		background-color: rgb(104, 110, 154);
+		background-color: rgb(104, 110, 154, 0.8);
 	}
 
 </style>
 
 <!-- Hero Start -->
 <section class="bg-half-170 d-table w-100"
-	style="background: url('<?= base_url(); ?>assets/images/profile/blow.png') center center; background-size:cover; background-position: center;">
-	<div class="bg-overlay"></div>
+	style="background: url('<?= base_url(); ?>assets/images/profile/blow.png') center center; min-height: 100vh; background-size:cover; background-position: center;">
+	<!-- <div class="bg-overlay"></div> -->
 	<div class="container">
-		<div class="row align-items-center">
-			<div class="col-lg-7 col-md-6">
-				<div class="title-heading mt-4">
-					<h1 class="display-4 font-weight-bold text-white title-dark mb-3">Booking tour <br> made easy</h1>
-					<p class="para-desc text-white-50">Launch your campaign and benefit from our expertise on designing
+		<div class="row justify-content-center text-center">
+			<div class="col-lg-12 col-md-12">
+				<div class="title-heading mt-4 p-3" style="background-color: rgb(255, 255, 255, 0.6);">
+					<h1 class="display-4 font-weight-light text-navy title-dark mb-3 text-uppercase font-optimus">
+						Tentang Kami</h1>
+					<p class="text-navy text-uppercase font-optimus">Launch your campaign and benefit
+						from our
+						expertise on
+						<br>designing
 						and managing conversion centered bootstrap4 html page.</p>
 				</div>
 			</div>
@@ -27,6 +31,6 @@
 <!--end section-->
 
 <section class="section"
-	style="background: url('<?= base_url(); ?>assets/images/profile/shape.png') center center; background-size:100%; background-position: center; z-index:1;">
+	style="background: url('<?= base_url(); ?>assets/images/profile/shape.png') center center; min-height: 400px; margin-top: -60px; background-size:cover; background-position: center; z-index:1;">
 	<br><br><br><br><br><br><br>
 </section>
