a3197929a5e2fb89c1837b49dca1ec2ac85e7233
muhadimulyana <muhammadadimulyana30@gmail.com>
2020-11-06 10:20:30 +0700

    fixing biji and career index page

--- application/views/career/index.php
+++ application/views/career/index.php
@@ -3,64 +3,84 @@
 		background-color: rgb(104, 110, 154, 0.65);
 	}
 
-    .text-xl-career {
-        font-size: 100px;
-    }
+	.text-xl-career {
+		font-size: 100px;
+	}
 
-    .bg-overlay-navy-300 {
-			background-color: rgba(15, 15, 140, 0.45);
-			/* background-color: rgba(60, 72, 88, 0.7); */
-			position: absolute;
-			top: 0;
-			right: 0;
-			left: 0;
-			bottom: 0;
-			width: 100%;
-			height: 100%;
+	.bg-overlay-navy-300 {
+		background-color: rgba(15, 15, 140, 0.45);
+		/* background-color: rgba(60, 72, 88, 0.7); */
+		position: absolute;
+		top: 0;
+		right: 0;
+		left: 0;
+		bottom: 0;
+		width: 100%;
+		height: 100%;
+	}
+
+	@media only screen and (max-width: 991px) {
+
+		.text-xl-career {
+			font-size: 50px;
+			margin-top: -30px !important;
 		}
+
+	}
 
 </style>
 
 
 <!-- Product Start -->
-<section class="section"
+<section class="section bg-half-260"
 	style="background-image:url('<?= base_url(); ?>assets/images/career/all.jpeg'); background-size: cover; min-height: 500px; background-position: center;">
 	<div class="bg-overlay-navy"></div>
-	<div class="container pb-lg-4 mb-md-5 mt-5">
-        <div class="row justify-content-center">
-            <div class="col-md-12 text-center">
-                <h1 class="heading text-white m-0">We Are <span class="text-xl-career text-white">Hiring</span></h1>
-                
-            </div>
-        </div>
-    </div>
+	<div class="container pb-lg-4">
+		<div class="row align-items-center">
+			<div class="col-md-12">
+				<h2 class="heading font-pala font-weight-light text-white">We are</h2>
+				<h1 class="text-xl-career font-optimus font-weight-light text-white" style="margin-top: -40px;">HIRING
+				</h1>
+
+			</div>
+		</div>
+	</div>
 </section>
 
-<section class="section bg-white" >
-    <div class="container p-lg-4">
-        <div class="row">
-            <div class="col-md-6 order-2 order-md-1">
-                <h5 class="text-navy">Gabung Bersama</h5>
-                <h1 class="text-navy">Pan Era Group</h5>
-                <p class="text-navy">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                <a href="javascript:void(0)" class="btn bg-navy btn-pills btn-xl text-white" style="background-color: #282c73 !important;">Daftar Lowongan</a>
-            </div>
-            <div class="col-md-6 order-1 order-md-2 mb-2 text-center">
-                <img src="<?= base_url(); ?>assets/images/career/vector.png" class="img-fluid" alt="">
-            </div>
-        </div>
-    </div>
+<section class="section bg-white">
+	<div class="container p-lg-4">
+		<div class="row">
+			<div class="col-md-6 order-2 order-md-1">
+				<h5 class="text-navy font-pala">Gabung Bersama</h5>
+				<h1 class="text-navy font-pala">Pan Era Group</h5>
+					<p class="text-navy font-pala">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+						eiusmod tempor
+						incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+						ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+						in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+					<a href="javascript:void(0)" class="font-pala btn bg-navy btn-pills btn-xl text-white"
+						style="background-color: #282c73 !important;">Daftar Lowongan</a>
+			</div>
+			<div class="col-md-6 order-1 order-md-2 mb-2 text-center">
+				<img src="<?= base_url(); ?>assets/images/career/vector.png" class="img-fluid" alt="">
+			</div>
+		</div>
+	</div>
 </section>
 
-<section class="section" style="background-image:url('<?= base_url(); ?>assets/images/career/work.jpeg'); background-size: cover; min-height: 500px; background-position: center;">
-    <div class="bg-overlay-navy-300"></div>
-    <div class="container justify-content-center p-5">
-        <div class="row text-center">
-            <div class="col-md-12">
-                <h5 class="text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur <br><br> Lectus sit amet est placerat in:</h5>
-                <a href="javascript:void(0)" class="btn btn-light btn-pills btn-lg">hrd@paneragroup.com</a><br>
-                <small class="text-white">Ut tellus elementum sagittis vitae</small>
-                <h5 class="text-white mt-3">Amet tellus cras adipiscing enim eu turpis egestas. Sed velit dignissim sodales ut eu sem integer vitae. Turpis nunc eget lorem dolor sed viverra ipsum nunc aliquet.</h5>
-        </div>
-    </div>
+<section class="section"
+	style="background-image:url('<?= base_url(); ?>assets/images/career/work.jpeg'); background-size: cover; min-height: 500px; background-position: center;">
+	<div class="bg-overlay-navy-300"></div>
+	<div class="container justify-content-center p-5">
+		<div class="row text-center">
+			<div class="col-md-12">
+				<h5 class="text-white">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+					aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur <br><br> Lectus sit amet est placerat in:</h5>
+				<a href="javascript:void(0)" class="btn btn-light btn-pills btn-lg">hrd@paneragroup.com</a><br>
+				<small class="text-white">Ut tellus elementum sagittis vitae</small>
+				<h5 class="text-white mt-3">Amet tellus cras adipiscing enim eu turpis egestas. Sed velit dignissim
+					sodales ut eu sem integer vitae. Turpis nunc eget lorem dolor sed viverra ipsum nunc aliquet.</h5>
+			</div>
+		</div>
 </section>
--- application/views/product/biji.php
+++ application/views/product/biji.php
@@ -14,7 +14,8 @@
 			<div class="col-lg-12 text-center">
 				<div class="page-next-level title-heading" style="background-color: rgba(255, 255, 255, 0.2);">
 					<h1 class="text-white title-dark pb-0 pt-5 mb-0 font-couture"> BIJI PLASTIK DAUR ULANG </h1>
-					<p class="text-white para-desc mb-5 pb-4 mx-auto font-pala">Produk ramah lingkungan yang menjadi inti dan
+					<p class="text-white para-desc mb-5 pb-4 mx-auto font-pala">Produk ramah lingkungan yang menjadi
+						inti dan
 						asal
 						identitas kami</p>
 				</div>
@@ -35,14 +36,15 @@
 	<div class="container">
 		<div class="row justify-content-center">
 			<div class="col-md-5 order-2 order-md-1">
-				<img src="<?= base_url(); ?>assets/images/biji_plastik/krung.png" class="img-fluid" style="max-height: 450px;"
-					alt="">
+				<img src="<?= base_url(); ?>assets/images/biji_plastik/krung.png" class="img-fluid"
+					style="max-height: 450px;" alt="">
 			</div>
 			<div class="col-md-7 order-1 order-md-2 pt-5">
 				<h1 style="line-height: 1;" class="text-navy heading mb-2 font-pala">Kami memproduksi dan
 					memasarkan biji
 					plastik</h1>
-				<h2 style="line-height: 1;" class="text-navy heading mb-3 font-pala">yang berasal dari hasil daur ulang limbah
+				<h2 style="line-height: 1;" class="text-navy heading mb-3 font-pala">yang berasal dari hasil daur ulang
+					limbah
 					plastik.
 				</h2>
 				<h5 style="line-height: 1;" class="text-navy heading mb-3 font-couture">Saat Ini Pan Era Group Hanya
@@ -50,7 +52,8 @@
 					Biji
 					Plastik Jenis Polyethylene Untuk Beragam Aplikasi Produksi
 				</h5>
-				<h5 style="line-height: 1;" class="text-navy heading mb-2  font-couture">Kestabilan Kualitas Produk Pan Era
+				<h5 style="line-height: 1;" class="text-navy heading mb-2  font-couture">Kestabilan Kualitas Produk Pan
+					Era
 					Group
 					Menjadikan
 					Pelanggan Dapat Menggunakan Produk Dengan Penuh Kepercayaan Dan Ketenangan
@@ -66,10 +69,11 @@
 			Quality Of
 			Our Products</h4>
 	</div>
-	<div class="container mt-lg-5 pt-lg-5">
+	<div class="container mt-5 pt-5">
 		<div class="row">
 			<div class="col-md-12 text-center">
-				<h4 style="line-height:1.5;" class="text-center font-couture text-navy text-uppercase">untuk info lebih lanjut
+				<h4 style="line-height:1.5;" class="text-center font-couture text-navy text-uppercase">untuk info lebih
+					lanjut
 					<br>
 					hubungi
 					departemen pemasaran kami</h4><br>
@@ -82,14 +86,15 @@
 <!--end section-->
 <!-- CTA End -->
 <!-- CTA Start -->
-<section class="section bg-cta"
+<section class="section bg-cta mb-5"
 	style="background: url('<?= base_url(); ?>assets/images/biji_plastik/backgrund 3.png'); height: 500px; background-size: cover;"
 	id="cta">
 	<div class="bg-overlay-navy"></div>
 	<div class="container justify-content-center">
 		<div class="row">
 			<div class="col-md-12 text-center">
-				<h4 style="line-height:1.5;" class="text-center text-white font-couture text-uppercase">apabila anda ingin
+				<h4 style="line-height:1.5;" class="text-center text-white font-couture text-uppercase">apabila anda
+					ingin
 					menyuplai limbah
 					plastik, dalam bentuk
 					apa pun,<br> silahkan hubungi departemen pembelian kami</h4><br>
