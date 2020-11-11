ca6faa9af1f67bea25c7ba5832965a2e3107aeb6
muhadimulyana <muhammadadimulyana30@gmail.com>
2020-11-11 16:44:20 +0700

    add owl carousel slider and fix header

--- application/views/profile/eri.php
+++ application/views/profile/eri.php
@@ -19,34 +19,42 @@
 		height: 100%;
 	}
 
+	@media only screen and (max-width: 991px) {
+
+		.text-comp {
+			font-size: 21px;
+		}
+
+	}
+
 </style>
 <!-- Product Start -->
 <section class="section"
-	style="background-image:url('<?= base_url(); ?>assets/images/profile/eri/mesin cuttng.jpg'); background-size: cover; min-height: 100vh; background-position: center;">
+	style="background-image:url('<?= base_url(); ?>assets/images/profile/eri.jpg'); background-size: cover; min-height: 100vh; background-position: center;">
 	<div class="bg-overlay-navy"></div>
 	<div class="container pb-lg-4 mb-md-5 mt-5">
 		<div class="row">
-			<div class="col-md-5">
-				<h3 class="p-2 text-white mb-3" style="background-color:rgb(125, 185, 232, 0.7); border-radius: 10px;">
+			<div class="col-md-2 text-center">
+				<img src="<?= base_url(); ?>assets/images/profile/eri.png" width="110px" class="img-fluid" alt="">
+			</div>
+			<div class="col-md-10 text-center">
+				<h1 class="p-2 text-white text-uppercase text-comp font-weight-light font-optimus mb-3">
 					PT. Elite Recycling Indonesia
-				</h3>
-				<h5 class="text-white font-weight-light">Jalan Raya Gemalapik No. 88 <br> Cikarang Selatan <br>
+				</h1>
+				<h5 class="text-serif font-weight-light text-white" style="margin-top: -30px">Lini Bisnis: Biji Plastik
+					Daur
+					Ulang</h5>
+			</div>
+			<div class="col-md-12 mt-5 mt-md-5 text-center text-md-left">
+				<h5 class="text-white font-weight-light font-pala">Lokasi <br> Jalan Raya Gemalapik No. 88 <br> Cikarang
+					Selatan
+					<br>
 					Kab. Bekasi
 					<br> Jawa Barat
 					17550 <br> Indonesia</h5>
-				<a href="javascript:void(0)" class="btn btn-light btn-pills mt-2">Open Maps</a>
-			</div>
-			<div class="col-md-3">
-			</div>
-			<div class="col-md-4 px-5 pt-5 text-center">
-				<div class="mt-5"></div>
-				<a href="javascript:void">
-					<img src="<?= base_url(); ?>assets/images/profile/eri.png" width="100px" class="img-fluid" alt="">
-				</a>
-				<a href="javascript:void">
-					<img src="<?= base_url(); ?>assets/images/profile/era.png" width="100px" class="img-fluid" alt="">
-				</a>
-				<a href="javascript:void(0)" class="btn btn-light btn-pills mt-2">Visi Dan Misi</a>
+				<div class="text-center">
+					<a href="javascript:void(0)" class="btn btn-light btn-pills mt-2 text-serif">Buka Dengan Maps</a>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -55,73 +63,94 @@
 <section class="section" style="background-color: #c8c6e7;">
 	<div class="container p-lg-4">
 		<div class="row">
-			<div class="col-md-6">
-				<h1 class="text-navy">Profil Perusahaan</h5>
-					<p class="text-navy">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+			<div class="col-md-12">
+				<h1 class="text-navy font-optimus font-weight-light text-uppercase mb-4" style="line-height: 1;">
+					Profil<br>Perusahaan</h5>
+					<p class="text-navy text-justify font-pala">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+						sed do
+						eiusmod tempor
 						incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
 						ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
 						in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-					<p class="text-navy">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+					<p class="text-navy text-justify font-pala">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+						sed do
+						eiusmod tempor
 						incididunt ut labore et dolore magna aliqua. Aliquam etiam erat velit scelerisque in dictum non
 						consectetur. Semper auctor neque vitae tempus quam pellentesque nec. Ultricies leo integer
 						malesuada nunc vel. Odio euismod lacinia at quis risus sed. Orci porta non pulvinar neque
 						laoreet suspendisse interdum consectetur libero. Urna nunc id cursus metus. Sagittis purus sit
 						amet volutpat consequat mauris nunc congue. Leo in vitae turpis massa. Massa tempor nec feugiat
 						nisl pretium fusce id velit.</p>
-			</div>
-			<div class="col-md-6 mb-2 text-center">
-				<div class="row align-items-center">
-					<div class="col-lg-4 col-4 mt-4 mt-lg-0 pt-2 pt-lg-0">
-
-					</div>
-					<!--end col-->
-
-					<div class="col-lg-8 col-8">
-						<div class="row">
-							<div class="col-lg-12 col-md-12 mt-4 mt-lg-0 pt-2 pt-lg-0">
-								<div class="card work-container work-modern overflow-hidden rounded border-0 shadow-md">
-									<div class="card-body p-0">
-										<img src="<?= base_url(); ?>assets/images/profile/era/gedung era.png"
-											class="img-fluid" alt="work-image">
-										<div class="overlay-work bg-dark"></div>
-										<div class="content">
-											<a href="javascript:void(0)"
-												class="title text-white d-block font-weight-bold">Lorem Ipsum</a>
-											<small class="text-light">Dolor Sit Amet</small>
-										</div>
-									</div>
-								</div>
-							</div>
-							<!--end col-->
-
-							<div class="col-lg-12 col-md-12 mt-4 pt-2">
-								<div class="card work-container work-modern overflow-hidden rounded border-0 shadow-md">
-									<div class="card-body p-0">
-										<img src="<?= base_url(); ?>assets/images/profile/era/slide3.jpg"
-											class="img-fluid" alt="work-image">
-										<div class="overlay-work bg-dark"></div>
-										<div class="content">
-											<a href="javascript:void(0)"
-												class="title text-white d-block font-weight-bold">Lorem Ipsum</a>
-											<small class="text-light">Dolor Sit Amet</small>
-										</div>
-									</div>
-								</div>
-							</div>
-							<!--end col-->
-						</div>
-						<!--end row-->
-					</div>
-					<!--end col-->
-				</div>
-				<!--end row-->
-			</div>
-			<div class="col-md-12">
-				<p class="text-navy">Elit sed vulputate mi sit amet mauris commodo quis imperdiet. Habitasse platea
-					dictumst vestibulum rhoncus. Consectetur a erat nam at. Mattis ullamcorper velit sed ullamcorper
-					morbi. Dolor purus non enim praesent elementum facilisis leo. Vestibulum morbi blandit cursus risus
-					at ultrices mi. Enim ut sem viverra aliquet eget sit amet tellus.</p>
+					<p class="text-navy text-justify font-pala">Elit sed vulputate mi sit amet mauris commodo quis
+						imperdiet.
+						Habitasse platea
+						dictumst vestibulum rhoncus. Consectetur a erat nam at. Mattis ullamcorper velit sed ullamcorper
+						morbi. Dolor purus non enim praesent elementum facilisis leo. Vestibulum morbi blandit cursus
+						risus
+						at ultrices mi. Enim ut sem viverra aliquet eget sit amet tellus.</p>
 			</div>
 		</div>
+		<div class="row">
+			<div class="col-12 mt-4 pt-2">
+				<div id="customer-testi" class="owl-carousel owl-theme">
+					<div class="card customer-testi text-center border rounded mx-2">
+						<div class="card-body">
+							<img src="images/client/amazon.svg" class="img-fluid avatar avatar-ex-sm mx-auto" alt="">
+							<p class="text-muted mt-4">" It seems that only fragments of the original text remain in the
+								Lorem Ipsum texts used today. "</p>
+							<h6 class="text-primary">- Thomas Israel</h6>
+						</div>
+					</div>
+					<div class="card customer-testi text-center border rounded mx-2">
+						<div class="card-body">
+							<img src="images/client/google.svg" class="img-fluid avatar avatar-ex-sm mx-auto" alt="">
+							<p class="text-muted mt-4">" The most well-known dummy text is the 'Lorem Ipsum', which is
+								said to have originated in the 16th century. "</p>
+							<h6 class="text-primary">- Carl Oliver</h6>
+						</div>
+					</div>
+					<div class="card customer-testi text-center border rounded mx-2">
+						<div class="card-body">
+							<img src="images/client/lenovo.svg" class="img-fluid avatar avatar-ex-sm mx-auto" alt="">
+							<p class="text-muted mt-4">" One disadvantage of Lorum Ipsum is that in Latin certain
+								letters appear more frequently than others. "</p>
+							<h6 class="text-primary">- Barbara McIntosh</h6>
+						</div>
+					</div>
+					<div class="card customer-testi text-center border rounded mx-2">
+						<div class="card-body">
+							<img src="images/client/paypal.svg" class="img-fluid avatar avatar-ex-sm mx-auto" alt="">
+							<p class="text-muted mt-4">" Thus, Lorem Ipsum has only limited suitability as a visual
+								filler for German texts. "</p>
+							<h6 class="text-primary">- Jill Webb</h6>
+						</div>
+					</div>
+					<div class="card customer-testi text-center border rounded mx-2">
+						<div class="card-body">
+							<img src="images/client/shopify.svg" class="img-fluid avatar avatar-ex-sm mx-auto" alt="">
+							<p class="text-muted mt-4">" There is now an abundance of readable dummy texts. These are
+								usually used when a text is required. "</p>
+							<h6 class="text-primary">- Dean Tolle</h6>
+						</div>
+					</div>
+					<div class="card customer-testi text-center border rounded mx-2">
+						<div class="card-body">
+							<img src="images/client/spotify.svg" class="img-fluid avatar avatar-ex-sm mx-auto" alt="">
+							<p class="text-muted mt-4">" According to most sources, Lorum Ipsum can be traced back to a
+								text composed by Cicero. "</p>
+							<h6 class="text-primary">- Christa Smith</h6>
+						</div>
+					</div>
+				</div>
+				<div class="slider_nav">
+					<button class="am-next btn btn-icon btn-sm btn-dark"><i data-feather="arrow-left"
+							class="icons"></i></button>
+					<button class="am-prev btn btn-icon btn-sm btn-dark"><i data-feather="arrow-right"
+							class="icons"></i></button>
+				</div>
+			</div>
+			<!--end col-->
+		</div>
+		<!--end row-->
 	</div>
 </section>
