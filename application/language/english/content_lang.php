b17f985272248eed870cd19023c84545bcb1087e
muhadimulyana <muhammadadimulyana30@gmail.com>
2021-01-26 08:42:25 +0700

    swap content language to indonesia default

--- application/language/english/content_lang.php
+++ application/language/english/content_lang.php
@@ -2,65 +2,63 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 // title
-$lang['text_title'] = 'Welcome to PAN ERA GROUP';
-$lang['text_code'] = 'en';
-$lang['text_nation'] = 'english';
-$lang['text_learn_more'] = 'Learn More';
-$lang['text_contact_us'] = 'Contact Us';
+$lang['text_title'] = 'Selamat Datang di PAN ERA GROUP';
+$lang['text_code'] = 'id';
+$lang['text_nation'] = 'indonesia';
+$lang['text_learn_more'] = 'Selengkapnya';
+$lang['text_contact_us'] = 'Hubungi Kami';
+$lang['text_det_contact'] = 'Detail Kontak';
+$lang['text_open_hour'] = 'Jam Buka';
 
-$lang['text_det_contact'] = 'Contact Details';
-$lang['text_open_hour'] = 'Opening Hours';
-
-$lang['placeholder_name'] = 'Name';
-$lang['placeholder_position'] = 'Position';
-$lang['placeholder_company'] = 'Company';
-$lang['placeholder_phone'] = 'Phone';
+$lang['placeholder_name'] = 'Nama';
+$lang['placeholder_position'] = 'Posisi';
+$lang['placeholder_company'] = 'Perusahaan';
+$lang['placeholder_phone'] = 'Telepon';
 $lang['placeholder_mail'] = 'Email';
-$lang['placeholder_message'] = 'Message';
-$lang['text_submit'] = 'Send';
-
-//
-$lang['text_read_more'] = 'Read More';
-
+$lang['placeholder_message'] = 'Pesan';
+$lang['text_submit'] = 'Kirim';
 
 //Quick Link
-$lang['qlink_home'] = 'Home';
-$lang['qlink_profile'] = 'Company Profile';
-$lang['qlink_vision'] = 'Vision & Mission';
-$lang['qlink_news'] = 'News';
-$lang['qlink_galeri'] = 'Gallery';
-$lang['qlink_pellet'] = 'Recycled Plastic Resin';
-$lang['qlink_bag'] = 'Plastics Bag';
-$lang['qlink_hr'] = 'Human Resource';
-$lang['qlink_career'] = 'Career';
-$lang['qlink_contact'] = 'Contact Us';
+$lang['qlink_home'] = 'Beranda';
+$lang['qlink_profile'] = 'Profil Perusahaan';
+$lang['qlink_vision'] = 'Visi & Misi';
+$lang['qlink_news'] = 'Berita';
+$lang['qlink_galeri'] = 'Galeri';
+$lang['qlink_pellet'] = 'Bijih Daur Ulang Plastik';
+$lang['qlink_bag'] = 'Kantong Plastik';
+$lang['qlink_hr'] = 'Manajemen SDM';
+$lang['qlink_career'] = 'Karir';
+$lang['qlink_contact'] = 'Kontak Kami';
 
 //Subscribe
-$lang['subs_us'] = '<b>Join us</b>, to receive the latest news and job information please submit your email address.';
-$lang['subs_mail'] = 'Your Email Adress';
+$lang['subs_us'] = '<b>Bergabung dengan kami</b>, untuk menerima berita terbaru dan informasi lowongan silakan submit alamat email anda.';
+$lang['subs_mail'] = 'Alamat Email Anda';
 
 //Home Page
-$lang['welcome_message'] = 'Welcome to PAN ERA GROUP';
-$lang['motto'] = 'We Are An Organization Dedicated To Nature, Oriented To Quality.';
-$lang['covid_message'] = 'The condition of the Covid-19 virus has made some of our distributors have to adjust their operating hours. But we are still committed to meeting your needs, please contact our telephone number to get a contact for our plastic product distributor.';
+$lang['welcome_message'] = 'Selamat Datang di PAN ERA GROUP';
+$lang['motto'] = 'Kami adalah organisasi yang berdedikasi pada alam, berorientasi pada kualitas.';
+$lang['covid_message'] = 'Kondisi virus Covid-19 membuat beberapa distributor kami harus menyesuaikan jam operasional. Namun kami tetap berkomitmen untuk memenuhi kebutuhan Anda, silahkan untuk menghubungi nomor telepon kami agar mendapatkan contact distributor produk plastik kami.';
 
-$lang['menu_profile'] = 'Company<br> Profile';
-$lang['menu_profile_detail'] = 'Introducing the companies that are members of the Pan Era Group';
+$lang['menu_profile'] = 'Profil<br> Perusahaan';
+$lang['menu_profile_detail'] = 'Pengenalan perusahaan - perusahaan yang tergabung dalam Pan Era Group';
 
-$lang['menu_vision'] = 'Vision & <br>Mission';
-$lang['menu_vision_detail'] = 'Our ambition to be a pioneer in plastic recycling industry has become our driving force to continue to progress and grow.';
+$lang['menu_vision'] = 'Visi & <br>Misi';
+$lang['menu_vision_detail'] = 'Ambisi kami yang kemudian menjadi daya dorong untuk terus maju dan berkembang';
 
-$lang['menu_product1'] = 'Recycled <br> Plastic Resin';
-$lang['menu_product1_detail'] = "Environmentally friendly recycled plastic resin has become Pan Era Group's core identityand origin.";
+$lang['menu_product1'] = 'Biji Plastik <br> Daur Ulang';
+$lang['menu_product1_detail'] = 'Produk ramah lingkungan yang menjadi inti dan asal identitas kami';
 
-$lang['menu_product2'] = 'Plastic <br> bag';
-$lang['menu_product2_detail'] = "LOCO Plastic Bag is Pan Era Group's plastic bag product and is one of our great pride.";
+$lang['menu_product2'] = 'Kantong <br> Plastik';
+$lang['menu_product2_detail'] = 'LOCO, kantong plastik kebanggaan besar kami yang kami kembangkan';
 
-$lang['menu_hr'] = 'Human <br> Resource Management';
-$lang['menu_hr_detail'] = 'Learn more about our human resource management philosophy and join us.';
+$lang['menu_hr'] = 'Manajemen <br> SDM';
+$lang['menu_hr_detail'] = 'Filosofi manajemen sumber daya manusia kami untuk berkembang bersama';
 
-$lang['menu_job'] = 'Career <br> Opportunities';
-$lang['menu_job_detail'] = 'Currently available positions and instructions on how to join Pan Era Group';
+$lang['menu_job'] = 'Kesempatan <br> Berkarir';
+$lang['menu_job_detail'] = 'Daftar lowongan posisi yang sedang tersedia beserta instruksi pelamaran';
 
-$lang['section_media'] = 'Media and Informations';
-$lang['section_media_detail'] = 'Latest News and Information';
+//
+$lang['text_read_more'] = 'Selengkapnya';
+
+$lang['section_media'] = 'Media and Informasi';
+$lang['section_media_detail'] = 'Berita dan Informasi Terbaru';
